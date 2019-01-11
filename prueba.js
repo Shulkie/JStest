6507c950eaa23ed7e3862d1b59ba0e2a1738e6d7
Yamila Krstulovich <yamlapin@icloud.com>
2019-01-11 15:02:15 -0300

    asdas

---
+++ prueba.js
@@ -0,0 +1,22 @@
+console.log("Hola mundo");
+
+var primerNombre = "Cosa \"La Cosa\" Simpson"; 
+
+console.log("bienvenide a JS, "+ primerNombre);
+
+var numero1 = 42;
+var numero2 = 25;
+
+var sonIguales = numero1 != numero2;
+console.log("numero son distintos", sonIguales);
+
+var numero3 = "42";
+var iguales = (numero1 === numero3);
+console.log(iguales);
+var valor1 = true;
+var valor2 = false;
+
+
+console.log("and", valor1 && valor2);
+console.log("or", valor1 || valor2);
+console.log("not", !valor1);
